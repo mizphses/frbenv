1ba941ddf02ca934ed375a0995eb6259880f62a3
なと <info@gentoo.app>
2020-12-19 09:38:47 +0900

    Update ubuntu_rbenv.sh

--- Linux/ubuntu_rbenv.sh
+++ Linux/ubuntu_rbenv.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 echo 'rubyのインストーラーを起動しています．しばらくお待ちください．sudoでの起動が必須です．'
 echo 'Wait a moment. We are waking up your ruby installer. You need to work this script with "sudo".'
-sudo apt -y install zlib1g-dev autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev libncurses5-dev libffi-dev libgdbm-dev libdb-dev openssl
+sudo apt -y install git zlib1g-dev autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libdb-dev openssl
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 export PATH="$HOME/.rbenv/bin:$PATH" >> ~/.bash_profile
